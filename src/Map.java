885e640d768d09449315df43c121475e732f35c6
QuintinKerns <qakern8560@ung.edu>
2019-03-25 14:09:36 -0400

    Map

--- src/Map.java
+++ src/Map.java
@@ -4,15 +4,29 @@ public class Map<KeyType, ValueType> {
 		
 	}
 	
-	public void put(KeyType key, ValueType val)
-	public ValueType get(KeyType key)
-	public boolean isEmpty()
-	public void makeEmpty()
+	public void put(KeyType key, ValueType val) {
+		items.insert(new Entry(key, val));
+	}
+	public ValueType get(KeyType key) {
+		return (ValueType) items.get(new Entry(key, null)).value;
+	}
+	public boolean isEmpty() {
+		if (items.capacity() == 0) return true;
+		else return false;
+	}
+	public void makeEmpty() {
+		items.makeEmpty();
+	}
 	
 	private QuadraticProbingHashTable<Entry<KeyType, ValueType>> items;
 	
 	private static class Entry<KeyType, ValueType>
 	{
+		private Entry(KeyType key, ValueType value){
+			this.key = key;
+			this.value = value;
+		}
+		
 		KeyType key;
 		ValueType value;
 		//appropriate constructions, etc.
